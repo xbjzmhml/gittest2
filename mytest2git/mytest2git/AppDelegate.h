d886260ff6ec63ea91ff513850612af78a9cd85e
apple <apple@appledeMacBook-Pro.local>
2016-05-09 11:15:44 +0800

    初始化项目

---
+++ mytest2git/mytest2git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mytest2git
+//
+//  Created by apple on 16/5/9.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
